13f9660f01de797dc4a0dd1ae2ff6c2ac99c27b4
maxnewton <newton0512@gmail.com>
2022-07-18 16:10:36 +0300

    -

--- fpw59/fpw59/urls.py
+++ fpw59/fpw59/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='word_count'),
+    path('', home, name='home'),
+    path('clear/', clean_data, name='clear'),
+    path('find/', find_word, name='find_word'),
 ]
--- fpw59/fpw59/views.py
+++ fpw59/fpw59/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
-def home(response):
-    return render(response, 'home.html', {})
+def read_f(my_file):
+    with my_file as f:
+        return [(word if isinstance(word, str) else word.decode()) for line in f for word in line.split() if word.isalpha()]  # составление списка слов из файла
 
+def home(request):
+    tmp_list = []
+    if request.method == 'POST':
+        tmp_list = read_f(request.FILES['file'])
+        with open("mydata.txt", "a") as myfile:
+            myfile.write(' '.join(tmp_list))
+        tmp_list = read_f(open("mydata.txt", "r"))
+    else:
+        tmp_list = read_f(open("mydata.txt", "r"))
+    return render(request, 'home.html', {'my_list': tmp_list, 'word': ''})
+
+def clean_data(request):
+    open('mydata.txt', 'w').close()
+    tmp_list = []
+    return render(request, 'home.html', {'my_list': tmp_list, 'word': ''})
+
+def find_word(request):
+    tmp_list = read_f(open("mydata.txt", "r"))
+    if request.method == 'POST':
+        word = request.POST.get('text1')
+        word_text = f'Слово {word} встречается в загруженных файлах {tmp_list.count(word)} раз(а).'
+    return render(request, 'home.html', {'my_list': tmp_list, 'word': word_text})
